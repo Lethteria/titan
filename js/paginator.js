cce71dd9f9e9edd1b6649f63e12ed7e1ea399445
Lethteria <Lethteria@gmail.com>
2017-06-07 13:14:19 +0300

    without Metinvest logo

--- js/paginator.js
+++ js/paginator.js
@@ -1,11 +1,64 @@
-﻿
-    function MyPagin(page,link){
-        var Id = link.attr('href').substr(1);
+﻿var MyPaginLink = {
+    LinkId: "",
+    LinkDisabled: false,
+    numOfElem: $('#pagin li').length,
+    GetId: function(link){
+        this.LinkId = link.attr('href').substr(1);
+        if (this.LinkId == 'prev-arrow' || this.LinkId == 'next-arrow'){
+            if ($(link).parent().hasClass('disabled')){
+                this.LinkDisabled = true;
+            }else {
+                var linkHref = $('#pagin').find('.active a').attr('href').substr(1),
+                    next = +linkHref.slice(-1) + 1,
+                    prev = +linkHref.slice(-1) - 1;
+                this.LinkDisabled = false;
+                if (this.LinkId == 'next-arrow'){
+                    return this.LinkId = linkHref.substring(0,linkHref.length-1)+next;
+                } else {return this.LinkId = linkHref.substring(0,linkHref.length-1)+prev;}
+            }
+        } else {
+            this.LinkDisabled = false;
+            return this.LinkId;
+        }
+    },
+    ActiveLink: function(){
+        if(!this.LinkDisabled){
+            $('#pagin').find('li').removeClass('active');
 
-        $('#page-wrap').append('<div class="progress"><div class="indeterminate"></div></div>');
-        $('html, body').animate({scrollTop: 0},700);
-        $('#page-wrap').load(page+'.html #'+Id);
-        link.parents('#pagin').find('li').removeClass('active');
-        link.parent().addClass('active');
+            var ActivHref = this.LinkId;
+
+            $('#pagin').find('a').each(function(){
+                if ($(this).attr('href') == "#"+ActivHref){
+                    $(this).parent().addClass('active');
+                }
+            });
+        }
+    },
+    LoadPage: function(page){
+        if(!this.LinkDisabled){
+            $('#page-wrap').append('<div class="progress"><div class="indeterminate"></div></div>');
+            $('html, body').animate({scrollTop: 0},700);
+            $('#page-wrap').load(page+'.html #'+this.LinkId);
+        }
+    },
+    AddDisabled: function(){
+        var LastLi = $('#pagin').find('li').eq(this.numOfElem-2),
+            FirstLi = $('#pagin').find('li').eq(1);
+        $('#pagin').find('li').removeClass('disabled');
+        if (LastLi.hasClass('active')){
+            LastLi.next().addClass('disabled');
+        }else {
+            if (FirstLi.hasClass('active')){
+                FirstLi.prev().addClass('disabled');
+            }
+        }
     }
+}
+
+function MyPagin(page,link){
+    MyPaginLink.GetId(link);
+    MyPaginLink.LoadPage(page);
+    MyPaginLink.ActiveLink();
+    MyPaginLink.AddDisabled();
+}
 
---
+++ js/search.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    $('#titan-search').click(function(){
+        $(this).val("Поиск временно не работает");
+        $(this).attr('disabled', true);
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $('#titan-search');
+        if (container.has(e.target).length === 0){
+            container.val(" ").attr('disabled', false);
+            //container.next('label').removeClass('active');
+        }
+    });
+});
